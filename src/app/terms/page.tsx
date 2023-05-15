4a2b7d4c1cdc04727472584daac8cb4aad063d2d
Sushant Kumar <sushantkumar23@gmail.com>
2023-05-15 11:56:14 +0530

    Added Terms of Service page

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,386 @@
+export default function Terms() {
+  return (
+    <div className="relative py-16 overflow-hidden bg-white">
+      <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+        <div
+          className="relative h-full mx-auto text-lg max-w-prose"
+          aria-hidden="true"
+        >
+          <svg
+            className="absolute transform translate-x-32 top-12 left-full"
+            width={404}
+            height={384}
+            fill="none"
+            viewBox="0 0 404 384"
+          >
+            <defs>
+              <pattern
+                id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x={0}
+                  y={0}
+                  width={4}
+                  height={4}
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width={404}
+              height={384}
+              fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
+            />
+          </svg>
+          <svg
+            className="absolute transform -translate-x-32 -translate-y-1/2 top-1/2 right-full"
+            width={404}
+            height={384}
+            fill="none"
+            viewBox="0 0 404 384"
+          >
+            <defs>
+              <pattern
+                id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x={0}
+                  y={0}
+                  width={4}
+                  height={4}
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width={404}
+              height={384}
+              fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+            />
+          </svg>
+          <svg
+            className="absolute transform translate-x-32 bottom-12 left-full"
+            width={404}
+            height={384}
+            fill="none"
+            viewBox="0 0 404 384"
+          >
+            <defs>
+              <pattern
+                id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x={0}
+                  y={0}
+                  width={4}
+                  height={4}
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width={404}
+              height={384}
+              fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
+            />
+          </svg>
+        </div>
+      </div>
+      <div className="relative max-w-2xl px-4 mx-auto text-gray-500 sm:px-6 lg:px-8">
+        <div className="mx-auto text-lg max-w-prose">
+          <h1>
+            <span className="block text-base font-semibold tracking-wide text-center text-indigo-600 uppercase">
+              Thoughts
+            </span>
+            <span className="block mt-2 text-3xl font-extrabold leading-8 tracking-tight text-center text-gray-900 sm:text-4xl">
+              Terms of Service
+            </span>
+          </h1>
+          <p className="mt-8">
+            By using our Products, Services &#38; Consultations, you are
+            agreeing to these terms. Please read them carefully.
+          </p>
+        </div>
+        <div className="mt-6 space-y-6 prose prose-lg prose-indigo">
+          <p>
+            <strong>Satria Technologies Private Limited</strong> would not be
+            liable for any misuse of the products delivered. After selling the
+            products &#38; services and the core technology, Satria Technologies
+            Private Limited will not be liable for any changs, maintenance and
+            errors which might arise due to external tampering. If the client
+            wants Satria Technologies Private Limited to handle the maintenance
+            of the product after selling the technology then there would be
+            extra charges which would be decided mutually between the two
+            parties in writing. Each and every communication related to the
+            technology and product would be considered valid for clients only if
+            they have the clauses mentioned in the agreement or have mails
+            approved from the higher authorities of Satria Technologies Private
+            Limited.
+          </p>
+          <h2 className="font-bold">Important Note:</h2>
+          <p>
+            The client can not sell the products or materials provided by Satria
+            Technologies Private Limited to another one without the written
+            permission of Satria Technologies Private Limited. In case of such a
+            condition, Satria Technologies Private Limited will have all the
+            authorities to stop all the services provided to the client on
+            immediate effect without any financial liability or losses.
+          </p>
+          <p>
+            In a situation of any dispute between the clients and their
+            customers, Satria Technologies Private Limited will not be liable
+            for any concern or financial losses for any third party company or
+            person.{" "}
+          </p>
+          <p>
+            Satria Technologies Private Limited will not have any responsibility
+            in case of giving money in the bank account of any other company.
+          </p>
+          <h3 className="font-bold">Quotations and Estimates:</h3>
+          <ul className="space-y-4" role="list">
+            <li>
+              All quotations and estimates provided by Satria Technologies
+              Private Limited are valid for a period of 30 days from date of
+              issue. Quotations not accepted within this timeframe must be
+              reissued.
+            </li>
+            <li>
+              All quoted prices, excluding where indicated, do not include Goods
+              and Services Tax.
+            </li>{" "}
+            <li>
+              Satria Technologies Private Limited reserves the right to suspend
+              the services/quotation at any time, without any prior information.
+            </li>
+          </ul>
+          <h3 className="font-bold">
+            Payment Terms &#38; Intellectual Property
+          </h3>
+          <ul className="space-y-4">
+            <li>
+              All quotations provided by Satria Technologies Private Limited,
+              require at least 10% deposit upon acceptance.
+            </li>{" "}
+            <li>
+              Unless prior arrangement has been made, final payment is strictly
+              net 15 days from the date of completion.
+            </li>{" "}
+            <li>
+              Any cost arising from payment clearings or transaction charges are
+              solely the responsibility of the client and will be charged as
+              such.
+            </li>{" "}
+            <li>
+              Satria Technologies Private Limited will only commence work on the
+              quoted application once any deposited funds have cleared.
+            </li>{" "}
+            <li>
+              The customer will not be entitled for any service in case of delay
+              in payment for more than 15 days from the final date of
+              installation / date of project / module completion.
+            </li>{" "}
+            <li>
+              If opted for service beyond the 12 months of maintenance period or
+              as agreed by Satria Technologies Private Limited, The Annual
+              Maintenance Charges (AMC) will be normally applicable 25%
+              (PERCENT) of original development cost of Project / Module; each
+              year the development cost for new modules will be added to the
+              initial development cost for the calculation of the AMC.
+            </li>{" "}
+            <li>
+              The AMC percentage shall be decided by the Satria Technologies
+              Private Limited; which depends upon the amount of efforts and work
+              required. This % may vary each year.
+            </li>{" "}
+            <li>
+              The recurring / renewal amount of the domain and hosting are
+              subjected to change as per the market rates. Satria Technologies
+              Private Limited disclaims all warranties or conditions, whether
+              expressed or implied, (including without limitation implied,
+              warranties or conditions of information and context). We consider
+              ourselves and intend to be subject to the jurisdiction only of the
+              courts of Gurugram, Haryana, India.
+            </li>{" "}
+            <li>
+              Unless specified, all quotations provided by Satria Technologies
+              Private Limited do not include any source code license.
+            </li>{" "}
+            <li>
+              All source code and associated intellectual property relating to
+              said source code, developed by Satria Technologies Private
+              Limited, solely remains the property of Satria Technologies
+              Private Limited, except where specific code license has been
+              issued to the client and said issuance has been indicated in
+              writing from Satria Technologies Private Limited.
+            </li>{" "}
+            <li>
+              Any alteration, reverse engineering, or manipulation of any kind
+              on the code, compiled or otherwise, created by Satria Technologies
+              Private Limited for the quoted application may be a breach of
+              trademark and copyright laws. If said breach is confirmed,
+              penalties will apply under the relevant acts.
+            </li>
+            Any costs incurred by Satria Technologies Private Limited for third
+            party code license required to complete the quoted application are
+            the responsibility of the client and will be solely borne by the
+            client. Any such third party code license will be attached to the
+            existing Satria Technologies Private Limited code license for the
+            quoted application.
+          </ul>
+          <h3 className="font-bold">Website Contents:</h3>{" "}
+          <ul className="space-y-4" role="list">
+            <li>
+              Clients are required to ensure that the content of the application
+              being quoted adheres to all the current Indian legislation
+              regarding publication.
+            </li>
+            <li>
+              Satria Technologies Private Limited reserves the right not to
+              include any material supplied by the client within the quoted
+              application if Satria Technologies Private Limited deems said
+              material inappropriate or offensive.erial inappropriate or
+              offensive.
+            </li>{" "}
+            <li>
+              It is the client&apos;s responsibility, in all cases, to ensure
+              the applications content is displayed and formatted as they
+              require. If the client cannot format the application&apos;s
+              content, Satria Technologies Private Limited will offer this
+              service at Satria Technologies Private Limited current hourly rate
+              at the time of the request.
+            </li>
+          </ul>
+          <h3 className="font-bold">Cancellations</h3>
+          <ul className="space-y-4" role="list">
+            <li>
+              Should the client wish to cancel acceptance of the quotation,
+              Satria Technologies Private Limited will invoice the client for
+              any work completed to date, as a percentage of the total work
+              involved.
+            </li>
+            <li>
+              The minimum cancellation fee will be 30% of the signed quotation.
+            </li>
+          </ul>
+          <h3 className="font-bold">Errors and Liabilities</h3>
+          <ul className="space-y-4">
+            <li>
+              Satria Technologies Private Limited will pursue due care to ensure
+              applications created by Satria Technologies Private Limited are
+              free of errors.
+            </li>{" "}
+            <li>
+              Satria Technologies Private Limited will correct any errors made
+              by Satria Technologies Private Limited staff in the undertaking of
+              the quoted application.
+            </li>{" "}
+            <li>
+              Satria Technologies Private Limited does not accept responsibility
+              for losses or damage arising from errors within any application.
+            </li>
+            <li>
+              Satria Technologies Private Limited does not accept responsibility
+              for errors, damages, losses or additional costs that relate to
+              third party products that Satria Technologies Private Limited may
+              require completing the quoted application.
+            </li>
+          </ul>
+          <h3 className="font-bold">Alterations</h3>
+          <ul className="space-y-4">
+            <li>
+              Any alterations requested by the client after development has
+              begun will incur extra development and regression testing time.
+              Dependent upon the alteration or change requested an average of 3
+              days extra development time per alteration should be allowed for.
+              The 3 day average may not be indicative of the time required and
+              can be extended commensurate of the time involved to implement
+              said changes.
+            </li>{" "}
+            <li>
+              Satria Technologies Private Limited will not accept responsibility
+              for any alterations performed by the client or any third party
+              which may cause or induce errors within the quoted application.
+            </li>{" "}
+            <li>
+              If Satria Technologies Private Limited are required to correct
+              said alterations or errors resulting from said alterations,
+              induced, injected or otherwise caused by parties other than Satria
+              Technologies Private Limited, the client will be charged at the
+              hourly rate that is current for Satria Technologies Private
+              Limited at the time said errors are to be fixed.
+            </li>
+          </ul>
+          <h3 className="font-bold">Completion of work</h3>
+          <ul className="space-y-4">
+            <li>
+              All timeframes offered by Satria Technologies Private Limited to
+              the client are estimates. The intrinsic nature of software
+              development and its intricacies do not offer Satria Technologies
+              Private Limited the luxury of defining definite timeframes.
+            </li>{" "}
+            <li>
+              Satria Technologies Private Limited will endeavour to complete all
+              work within the estimated time frames discussed with the client in
+              the quotation. However, Satria Technologies Private Limited will
+              not be liable for any penalties, monies or hardships otherwise
+              incurred by the client if the application cannot be delivered
+              within the estimated timeframe.
+            </li>{" "}
+            <li>
+              Satria Technologies Private Limited will not release the quoted
+              application unless all payments have been met under the
+              obligations of the quotation or work agreement.
+            </li>{" "}
+            <li>
+              If Satria Technologies Private Limited does not have control over
+              the residence where the finished work will reside then full
+              payment must be made prior to said work being released by Satria
+              Technologies Private Limited.
+            </li>{" "}
+            <li>
+              The quoted application remains the property of Satria Technologies
+              Private Limited Software until all obligations have been met for
+              release of said application to the client.
+            </li>{" "}
+            <li>
+              If Satria Technologies Private Limited is working as a third party
+              to another company, said company is responsible in meeting the
+              obligations for release of the quote application to their client.
+            </li>
+          </ul>
+          <h2 className="font-bold">
+            Changes to site and these terms and conditions
+          </h2>
+          <p>
+            This Site and these Satria Technologies Private Limited Terms and
+            Conditions may be amended, revised, changed, updated or modified by
+            Satria Technologies Private Limited with or without notice. Please
+            review this link on a regular basis for changes. Continued use of
+            this Site following any change to the Satria Technologies Private
+            Limited Terms and Conditions constitutes your acceptance of any such
+            change to the Satria Technologies Private Limited Terms and
+            Conditions.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
