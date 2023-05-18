659691226d7fd4fb1a4ca2f24cb96fe08534d5d2
Sushant Kumar <sushantkumar23@gmail.com>
2023-05-18 13:15:49 +0530

    Added handle storage before calling API

--- src/app/api/handles/route.ts
+++ src/app/api/handles/route.ts
@@ -6,10 +6,18 @@ import type { Profile, Post } from "@/types"
 export async function POST(request: Request) {
   const { handle } = await request.json()
 
+  const db = (await clientPromise).db()
+  await db
+    .collection("handles")
+    .updateOne(
+      { handle: handle },
+      { $set: { handle: handle } },
+      { upsert: true }
+    )
+
   const res = await fetch(`https://instagram-api.scribo.dev/api/${handle}`)
   const posts: Post[] = await res.json()
 
-  const db = (await clientPromise).db()
   const updatedPosts: Post[] = posts.map((post) => {
     return { ...post, handle: handle }
   })
